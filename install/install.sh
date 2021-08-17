c4ef024efb1fce5d69d7392b6bbc97d5e4358549
jordan <jordankhn8@gmail.com>
2021-08-16 23:38:22 -0500

    **

--- install/install.sh
+++ install/install.sh
@@ -5,11 +5,11 @@ cd /home/pi
 sudo pip install uwsgi
 git clone https://github.com/fastcarsub88/keypad_wpi.git
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/pi/keypad/install/nginx_conf /etc/nginx/sites-enabled/
+sudo ln -s /home/pi/keypad_wpi/install/nginx_conf /etc/nginx/sites-enabled/
 sudo nginx -s reload
-sudo systemctl link /home/pi/keypad/install/wiegand_web.service
-sudo systemctl link /home/pi/keypad/install/wiegand_reader.service
-sudo systemctl link /home/pi/keypad/install/wiegand_schedule.service
+sudo systemctl link /home/pi/keypad_wpi/install/wiegand_web.service
+sudo systemctl link /home/pi/keypad_wpi/install/wiegand_reader.service
+sudo systemctl link /home/pi/keypad_wpi/install/wiegand_schedule.service
 sudo systemctl enable pigpiod
 sudo systemctl enable wiegand_web
 sudo systemctl enable wiegand_reader
